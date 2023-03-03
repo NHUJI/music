9f70c7f87fb2f47d356916850d559fa980a83e33
NHUJI <cnzhouchang@gmail.com>
2023-03-03 14:09:07 +0800

    init 初始化项目

---
+++ .eslintrc.cjs
@@ -0,0 +1,22 @@
+/* eslint-env node */
+require('@rushstack/eslint-patch/modern-module-resolution')
+
+module.exports = {
+  root: true,
+  extends: [
+    'plugin:vue/vue3-essential',
+    'eslint:recommended',
+    // 这一条避免与prettier冲突
+    '@vue/eslint-config-prettier'
+    // '@vue/eslint-config-prettier/skip-formatting'
+  ],
+  overrides: [
+    {
+      files: ['cypress/e2e/**/*.{cy,spec}.{js,ts,jsx,tsx}'],
+      extends: ['plugin:cypress/recommended']
+    }
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest'
+  }
+}
