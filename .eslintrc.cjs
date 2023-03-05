3de0b20c132da1cf8eb2fe99dc6091e8f52158d3
NHUJI <cnzhouchang@gmail.com>
2023-03-05 12:40:57 +0800

    即将准备添加pinia的stores

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -6,9 +6,7 @@ module.exports = {
   extends: [
     'plugin:vue/vue3-essential',
     'eslint:recommended',
-    // 这一条避免与prettier冲突
-    '@vue/eslint-config-prettier'
-    // '@vue/eslint-config-prettier/skip-formatting'
+    '@vue/eslint-config-prettier/skip-formatting'
   ],
   overrides: [
     {
@@ -18,5 +16,8 @@ module.exports = {
   ],
   parserOptions: {
     ecmaVersion: 'latest'
+  },
+  env: {
+    node: true
   }
 }
