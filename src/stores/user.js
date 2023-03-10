c864ec7ee8aef945511033898dfdc186799e2bdc
NHUJI <cnzhouchang@gmail.com>
2023-03-10 11:27:28 +0800

    修改:按照user uid存入数据库user中

--- src/stores/user.js
+++ src/stores/user.js
@@ -1,6 +1,7 @@
 // 记录用户登录状态
 import { defineStore } from "pinia";
-import { auth, usersCollection } from "@/includes/firebase";
+import { auth, usersCollection } from "@/includes/firebase"; //usersCollection是我们自己定义的名称,其实是firestore.db.user表
+import { updateProfile } from "@firebase/auth";
 
 export default defineStore("user", {
   state: () => ({
@@ -8,19 +9,25 @@ export default defineStore("user", {
   }),
   actions: {
     async register(values) {
-      await auth.createUserWithEmailAndPassword(
+      const userCred = await auth.createUserWithEmailAndPassword(
         values.email,
         values.password
       );
+      console.log(userCred, userCred.user.uid);
 
-      await usersCollection.add({
+      await usersCollection.doc(userCred.user.uid).set({
         name: values.name,
         email: values.email,
         age: values.age,
         country: values.country,
       });
 
+      await updateProfile(userCred.user, {
+        displayName: values.name,
+      });
+
       this.userLoggedIn = true;
+      // 错误的处理在appAuth.vue中
     },
   },
 });
