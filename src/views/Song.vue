91fc7176af2929bf12e790b002cc5c6631e4d7b7
NHUJI <cnzhouchang@gmail.com>
2023-03-19 18:40:25 +0800

    add: 评论排序

--- src/views/Song.vue
+++ src/views/Song.vue
@@ -65,6 +65,7 @@
 
         <!-- Sort Comments -->
         <select
+          v-model="sort"
           class="block mt-4 py-1.5 px-3 text-gray-800 border border-gray-300 transition duration-500 focus:outline-none focus:border-black rounded"
         >
           <option value="1">Latest</option>
@@ -76,76 +77,19 @@
 
   <!-- Comments -->
   <ul class="container mx-auto">
-    <li class="p-6 bg-gray-50 border border-gray-200">
+    <li
+      class="p-6 bg-gray-50 border border-gray-200"
+      v-for="comment in sortedComments"
+      :key="comment.docID"
+    >
       <!-- Comment Author -->
       <div class="mb-5">
-        <div class="font-bold">Elaine Dreyfuss</div>
-        <time>5 mins ago</time>
+        <div class="font-bold">{{ comment.name }}</div>
+        <time>{{ comment.datePosted }}</time>
       </div>
 
       <p>
-        Sed ut perspiciatis unde omnis iste natus error sit voluptatem
-        accusantium der doloremque laudantium.
-      </p>
-    </li>
-    <li class="p-6 bg-gray-50 border border-gray-200">
-      <!-- Comment Author -->
-      <div class="mb-5">
-        <div class="font-bold">Elaine Dreyfuss</div>
-        <time>5 mins ago</time>
-      </div>
-
-      <p>
-        Sed ut perspiciatis unde omnis iste natus error sit voluptatem
-        accusantium der doloremque laudantium.
-      </p>
-    </li>
-    <li class="p-6 bg-gray-50 border border-gray-200">
-      <!-- Comment Author -->
-      <div class="mb-5">
-        <div class="font-bold">Elaine Dreyfuss</div>
-        <time>5 mins ago</time>
-      </div>
-
-      <p>
-        Sed ut perspiciatis unde omnis iste natus error sit voluptatem
-        accusantium der doloremque laudantium.
-      </p>
-    </li>
-    <li class="p-6 bg-gray-50 border border-gray-200">
-      <!-- Comment Author -->
-      <div class="mb-5">
-        <div class="font-bold">Elaine Dreyfuss</div>
-        <time>5 mins ago</time>
-      </div>
-
-      <p>
-        Sed ut perspiciatis unde omnis iste natus error sit voluptatem
-        accusantium der doloremque laudantium.
-      </p>
-    </li>
-    <li class="p-6 bg-gray-50 border border-gray-200">
-      <!-- Comment Author -->
-      <div class="mb-5">
-        <div class="font-bold">Elaine Dreyfuss</div>
-        <time>5 mins ago</time>
-      </div>
-
-      <p>
-        Sed ut perspiciatis unde omnis iste natus error sit voluptatem
-        accusantium der doloremque laudantium.
-      </p>
-    </li>
-    <li class="p-6 bg-gray-50 border border-gray-200">
-      <!-- Comment Author -->
-      <div class="mb-5">
-        <div class="font-bold">Elaine Dreyfuss</div>
-        <time>5 mins ago</time>
-      </div>
-
-      <p>
-        Sed ut perspiciatis unde omnis iste natus error sit voluptatem
-        accusantium der doloremque laudantium.
+        {{ comment.comment }}
       </p>
     </li>
   </ul>
@@ -173,10 +117,37 @@ export default {
       comment_alert_variant: "bg-blue-500",
       comment_alert_msg:
         "Please wait! Your comment is being submitted... ",
+      comments: [],
+      sort: "1", // 1: latest, 2: oldest
     };
   },
   computed: {
     ...mapState(useUserStore, ["userLoggedIn"]),
+    sortedComments() {
+      // 不能直接用 this.comments.sort()，计算属性可以使用数据属性,但不应该直接修改,所以要用 slice() 复制一份
+      return this.comments.slice().sort((a, b) => {
+        if (this.sort === "1") {
+          // use Date(b.datePosted) make String to Object
+          return new Date(b.datePosted) - new Date(a.datePosted);
+        } else {
+          return new Date(a.datePosted) - new Date(b.datePosted);
+        }
+      });
+    },
+  },
+  async created() {
+    // not use where because only want to get one document, if we want to get multiple documents, we use where
+    const docSnapshot = await songsCollection
+      .doc(this.$route.params.id)
+      .get();
+
+    if (!docSnapshot.exists) {
+      this.$router.push({ name: "home" });
+      return;
+    }
+
+    this.song = docSnapshot.data();
+    this.getComments();
   },
   methods: {
     async addComment(values, { resetForm }) {
@@ -196,25 +167,29 @@ export default {
 
       await commentsCollection.add(comment); // add comment to firestore
 
+      this.getComments();
+
       this.comment_in_submission = false;
       this.comment_alert_variant = "bg-green-500";
       this.comment_alert_msg =
         "Your comment has been submitted successfully!";
       resetForm(); // clear form
     },
-  },
-  async created() {
-    // not use where because only want to get one document, if we want to get multiple documents, we use where
-    const docSnapshot = await songsCollection
-      .doc(this.$route.params.id)
-      .get();
+    async getComments() {
+      const querySnapshot = await commentsCollection
+        .where("sid", "==", this.$route.params.id)
+        .get();
 
-    if (!docSnapshot.exists) {
-      this.$router.push({ name: "home" });
-      return;
-    }
+      this.comment = []; // clear comment array
 
-    this.song = docSnapshot.data();
+      querySnapshot.forEach((doc) => {
+        // add comment to array
+        this.comments.push({
+          docID: doc.id, // id not in the document, so we need to add it
+          ...doc.data(),
+        });
+      });
+    },
   },
 };
 </script>
